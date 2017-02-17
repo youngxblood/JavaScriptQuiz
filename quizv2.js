8a485b7341b5a39475ea94ef26a1c565e8dbd230
youngxblood <crazyeight68@gmail.com>
2017-02-17 07:24:45 -0800

    Adding new "quizv2.js" file to start reworking the quiz architecture. Also updating the index.html to properly reference the used files.
    Signed-off-by: youngxblood <crazyeight68@gmail.com>

---
+++ quizv2.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Mark on 2/17/2017.
+ */
+
+var questionArray = [
+    ['Which animal lives longer? Cats or dogs.', 'cats'],
+    ['How many legs do spiders have?', 'eight'],
+    ['What type of class of animal are humans considered?', 'mammals'],
+    ['What is the name of the narrator for BBC\'s series "Planet Earth"?', 'david attenborough'],
+    ['Which is more severe of a classification? Endangered or extinct?', 'extinct']
+]
